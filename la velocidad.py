d4dad8d53e18c13e6cd9fc0cbe1dc0b39a08abbb
mariagl03 <115078373+mariagl03@users.noreply.github.com>
2022-10-05 14:26:06 +0200

    Delete la velocidad.py

--- la velocidad.py
+++
@@ -1,6 +0,0 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Este programa calcula la velocidad en kilometros por hora a partir de los metros por segundo
-... metros = float(input("Introduce la distancia en metros:"))
-... tiempo = float(input("Introduce el tiempo en segundos:"))
-... velocidad = (metros/1000) / (tiempo/3600)
